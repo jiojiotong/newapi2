43bcf0e7d65c67b11c65830108d6e29857307e6d
1 <a1@1deiMac-Pro.local>
2026-05-22 13:47:30 +0800

    Stabilize Swift package CI build

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
     name: "NewAPIAdmin",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v14)
+        .iOS(.v16),
+        .macOS(.v13)
     ],
     products: [
         .library(name: "NewAPIAdmin", targets: ["NewAPIAdmin"])
